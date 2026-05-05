b21ed33f0dc6e8d77424d4963d67431f3f908c7e
GreeshmaGR <grgreeshma1@gmail.com>
2026-05-05 13:02:42 +0530

    Add files via upload
    learndatatypes

---
+++ Chrome.java
@@ -0,0 +1,40 @@
+package org.testleaf.week1.day1;
+
+public class Chrome {
+
+    //declare main method
+    public static void main(String[] args) {
+        //float-to write decimal numbers
+
+         float value =91.0f;
+
+         //string- a class variable always starts with uppercase
+         //for string use "" to wite code
+         String browser=(" Google");
+
+         //boolean 
+         boolean notABrowser=false;
+
+         //int
+         int yearOfRelease=2008;
+
+         //character
+         char key='C';
+
+         //print using sysout
+         //always print in double quote
+         System.out.println("Google Chrome");
+         System.out.println("google browser" +yearOfRelease);
+         
+
+
+
+
+
+
+
+
+
+    }
+
+}
