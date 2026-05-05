2d2f338f6b4e1a9965b76d12c9611bb8d5a28f29
GreeshmaGR <grgreeshma1@gmail.com>
2026-05-05 12:42:43 +0530

    Add files via upload
    week1.day1

---
+++ FirstProgram.java
@@ -0,0 +1,17 @@
+package org.testleaf.week1.day1;
+//public -access modifier
+//class-keyword
+//firstprogram-classname
+
+public class FirstProgram {
+//declare main method
+    public static void main(String[] args) {
+        
+        //to print the code system.out.println
+        System.out.println("Testleaf program");
+
+    }
+
+
+
+}
