a5d5b5e5f32a6d42cad23d95a9d7e2489ae8e204
GreeshmaGR <grgreeshma1@gmail.com>
2026-05-08 23:34:14 +0530

    week2.day1 assignment

---
+++ Library.java
@@ -0,0 +1,55 @@
+package org.testleaf.week2.day1;
+
+public class Library {
+//declare a method name
+    public String addBook(String bookTitle){
+        //print the statement
+
+        System.out.println("Book Added Sucessfully");
+        return bookTitle;
+    }
+    //another method defining
+    public void issueBook() {
+        //print statemnt
+        System.out.println("Book Issued Successfully");
+    
+    }
+ public static void main(String[] args) {
+
+    Library options=new Library();
+    options.addBook("JAVA");
+    options.issueBook();
+
+    
+ }
+
+}
+    
+
+
+
+
+    
+ 
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
